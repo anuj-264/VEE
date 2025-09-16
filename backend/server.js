5eb933a242601a38cea46eeafb45d93d63e0314f
Anuj Yadav <anuj264kumar@gmail.com>
2025-09-16 09:03:54 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import cors from 'cors';
 const app = express();
 app.use(cors(
     {
-        origin: process.env.CORS_ORIGIN,
+        origin: "http://localhost:5173",
         credentials: true
     }
 ))
